00d8d93d91a476af3fab19c77a854219b0eb62a3
crumbjp <hiroaki.kubota@mail.crumb.jp>
2018-01-10 09:28:22 +0900

    Remove VALID_LANGUAGES

--- lib/wikicloth/extensions/source.rb
+++ lib/wikicloth/extensions/source.rb
@@ -6,11 +6,11 @@ end
 module WikiCloth
   class SourceExtension < Extension
 
-    VALID_LANGUAGES = [ 'as','applescript','arm','asp','asm','awk','bat','bibtex','bbcode','bison','lua',
-      'bms','boo','c','c++','cc','cpp','cxx','h','hh','hpp','hxx','clojure','cbl','cob','cobol','cfc','cfm',
-      'coldfusion','csharp','cs','css','d','diff','patch','erlang','erl','hrl','go','hs','haskell','html',
-      'htm','xml','xhtml','httpd','js','javascript','matlab','m','perl','cgi','pl','plex','plx','pm','php',
-      'php3','php4','php5','php6','python','py','ruby','rb', 'text' ]
+    # VALID_LANGUAGES = [ 'as','applescript','arm','asp','asm','awk','bat','bibtex','bbcode','bison','lua',
+    #   'bms','boo','c','c++','cc','cpp','cxx','h','hh','hpp','hxx','clojure','cbl','cob','cobol','cfc','cfm',
+    #   'coldfusion','csharp','cs','css','d','diff','patch','erlang','erl','hrl','go','hs','haskell','html',
+    #   'htm','xml','xhtml','httpd','js','javascript','matlab','m','perl','cgi','pl','plex','plx','pm','php',
+    #   'php3','php4','php5','php6','python','py','ruby','rb', 'text' ]
 
     # <source lang="language">source code</source>
     #
@@ -25,7 +25,7 @@ module WikiCloth
       error = nil
 
       raise I18n.t("lang attribute is required") unless buffer.element_attributes.has_key?('lang')
-      raise I18n.t("unknown lang", :lang => buffer.element_attributes['lang'].downcase) unless SourceExtension::VALID_LANGUAGES.include?(buffer.element_attributes['lang'].downcase)
+      # raise I18n.t("unknown lang", :lang => buffer.element_attributes['lang'].downcase) unless SourceExtension::VALID_LANGUAGES.include?(buffer.element_attributes['lang'].downcase)
 
       if defined?(Pygments)
 	begin
